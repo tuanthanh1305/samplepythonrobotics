8d2ec6442743bcd57600d3bae97fdb90778e3b07
Tran Tuan Thanh <130335648+tuanthanh1305@users.noreply.github.com>
2023-10-13 14:24:10 +0700

    Update screen_saver_update.py

--- screen_saver_update.py
+++ screen_saver_update.py
@@ -1,3 +1,12 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Tuan Thanh Tran                                              #
+# 	Created:      10/12/2023, 4:26:13 PM                                       #
+# 	Description:  screen_saver_update IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
 from vex import *
 import random
 import math
@@ -15,13 +24,9 @@ def screen_saver():
         side_length = random.randint(2, 15)
         x = random.randint(0, 159)
         y = random.randint(0, 107)
-        
-        # Vẽ hình vuông xoay tròn đổi màu liên tục
         for angle in range(0, 360, 10):
             x_offset = side_length * math.cos(math.radians(angle))
             y_offset = side_length * math.sin(math.radians(angle))
             screen.draw_rectangle(x, y, side_length, side_length, angle)
             wait(20, MSEC)
-            screen.draw_rectangle(x, y, side_length, side_length, angle, color=Color.BLACK)  # Xóa hình trước khi xoay
-
 screen_saver()
